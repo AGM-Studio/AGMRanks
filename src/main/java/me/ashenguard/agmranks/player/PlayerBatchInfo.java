61a2ac1a0881be3e1188bdcf65c6f6491d0c4813
Ashenguard <myminecraft2019@gmail.com>
2022-08-07 16:50:27 +0430

    6.0.0 - RankedPlayer & Info

---
+++ src/main/java/me/ashenguard/agmranks/player/PlayerBatchInfo.java
@@ -0,0 +1,66 @@
+package me.ashenguard.agmranks.player;
+
+import me.ashenguard.agmranks.Utils;
+import me.ashenguard.agmranks.ranks.Rank;
+import me.ashenguard.agmranks.ranks.RankBatch;
+import org.bukkit.persistence.PersistentDataType;
+
+public class PlayerBatchInfo {
+    private final RankedPlayer ranked;
+    private final RankBatch batch;
+
+    private long score;
+    private Rank current;
+    private Rank highest;
+    private int prestige;
+
+    protected PlayerBatchInfo(RankedPlayer ranked, RankBatch batch) {
+        this.ranked = ranked;
+        this.batch = batch;
+
+        score = Utils.getPlayerData(ranked.player, batch.getScorePDCKey(), PersistentDataType.LONG, (long) 0);
+        current = getRankFromKey(batch.getRankPDCKey());
+        highest = getRankFromKey(batch.getHighRankPDCKey());
+        prestige = Utils.getPlayerData(ranked.player, batch.getPrestigePDCKey(), PersistentDataType.INTEGER, 0);
+    }
+
+    private Rank getRankFromKey(String key) {
+        int id = Utils.getPlayerData(ranked.player, key, PersistentDataType.INTEGER, -1);
+        if (0 > id || id >= batch.getRanks().size()) return null;
+        return batch.getRanks().get(id);
+    }
+
+    public long getScore() {
+        return score;
+    }
+    public void setScore(long score) {
+        Utils.setPlayerData(ranked.player, batch.getScorePDCKey(), PersistentDataType.LONG, score);
+        this.score = score;
+    }
+
+    public Rank getRank() {
+        return current;
+    }
+    public void setRank(Rank rank) {
+        if (rank.getBatch() != batch) return;
+        Utils.setPlayerData(ranked.player, batch.getRankPDCKey(), PersistentDataType.INTEGER, rank.getID());
+        this.current = rank;
+    }
+
+    public Rank getHighRank() {
+        return highest;
+    }
+    public void setHighRank(Rank rank, boolean overwrite) {
+        if (rank.getBatch() != batch) return;
+        Utils.setPlayerData(ranked.player, batch.getHighRankPDCKey(), PersistentDataType.INTEGER, rank.getID(), overwrite ? null : integer -> integer != null && integer < rank.getID());
+        this.highest = rank;
+    }
+
+    public int getPrestige() {
+        return prestige;
+    }
+    public void setPrestige(int prestige) {
+        Utils.setPlayerData(ranked.player, batch.getPrestigePDCKey(), PersistentDataType.INTEGER, prestige);
+        this.prestige = prestige;
+    }
+}
---
+++ src/main/java/me/ashenguard/agmranks/player/RankedPlayer.java
@@ -0,0 +1,28 @@
+package me.ashenguard.agmranks.player;
+
+import me.ashenguard.agmranks.ranks.RankBatch;
+import me.ashenguard.agmranks.ranks.RankManager;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RankedPlayer {
+    public static RankedPlayer get(Player player) {
+        return new RankedPlayer(player);
+    }
+
+    protected final Player player;
+    protected final Map<RankBatch, PlayerBatchInfo> batches = new HashMap<>();
+
+    protected RankedPlayer(Player player) {
+        this.player = player;
+
+        for (RankBatch batch: RankManager.getBatches().values())
+            if (batch.hasPermission(player)) batches.put(batch, new PlayerBatchInfo(this, batch));
+    }
+
+    public PlayerBatchInfo getBatchInfo(RankBatch batch) {
+        return batches.get(batch);
+    }
+}
